9d841d5b2d407e99c1ef2d85e92064a747225786
123mufee <fathimamufeedakaruvally@gmail.com>
2022-12-18 11:10:55 +0530

    deploy

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state = {
   db: null,
 };
 module.exports.connect = function (done) {
-  const url = "mongodb://localhost:27017";
+  const url = "mongodb+srv://Mufeeda:PtALvv8GTdAecA6H@cluster0.kuevxxd.mongodb.net/?retryWrites=true&w=majority";
   const dbname = "BStyle";
 
   mongoClient.connect(url, (err, data) => {
